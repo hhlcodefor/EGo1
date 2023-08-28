ff7bdc01b64e75c9d3e62c79673ce113ef866e72
hhlcodefor <huhao2336289144@qq.com>
2023-08-28 13:30:00 +0800

    clock_v1.1
    添加了按钮功能，可以通过按钮加减单位时间，但是加法模块有些问题，好像是s4和s0按键关联了

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 
 ## 运行时的问题
 
- 8/27设置初值问题
+ 8/27<br>
+ 编程设置初值问题<br>
+ 8/28<br>
+ 按键抖动问题<br>
+ 单位时间加模块功能重复且非法调用，可能是数值溢出的问题-[Synth 8-2292] literal value truncated to fit in 2 bits ["D:/vivado/clock/clock.srcs/sources_1/new/clock.v":115]
+<br>
+
 
 ## 版本日志
 8/27 v1.0 <br>
--- clock.v
+++ clock.v
@@ -23,7 +23,8 @@
 module clock(
 	input clk,	//系统时钟
 	input rst,	//复位
-	//input [4:0]key,	//键盘输入
+	input [4:0]key,	//键盘输入
+
 	output [7:0]seg_data1, //第一个四位数码管显示数据
 	output [7:0]seg_data2,	//第二个四位数码管显示数据
 	output [7:0]seg_which	//数码管位选信号
@@ -41,6 +42,15 @@ module clock(
 	reg [7:0] smg=0;	//承载一位数码管的段码
 	reg pulse=1'b0;	//上升沿作为500Hz扫频
 
+	parameter xd=21'd2000000;	//削抖计时20ms
+	reg [20:0] cnt_xd=0;		//削抖记录
+	reg [1:0]key_data=0;	//按键数据//十进制//就是板子上按键的标号
+	reg state=0;		//时钟模式//状态机
+	reg [25:0]cnt_trinkle=0;	//手动调节时间功能//闪烁记录
+	reg [1:0]change_which=0;	//手动调节时间功能//调节时间类型，时？分？秒？
+	reg shine = 1'b0;	//数码管暗灭状态
+
+	///////////////////////////////////////////////////////////////    时钟逻辑部分
 	always @(posedge clk)	//计时1s
 		if(cnt_1s==T1S)
 		cnt_1s<=27'd0;
@@ -52,7 +62,9 @@ module clock(
 		begin
 			if(!rst)
 			second=0;
-			else if(cnt_1s==T1S)
+			else if (state==0)	//时钟逻辑状态
+			begin
+			if(cnt_1s==T1S)
 			second=second+1;	//秒计时
 			else if(second==60)
 				begin
@@ -72,8 +84,122 @@ module clock(
 				end
 			else
 			second=second;
+			end
+			else
+		begin
+			case (key_data)
+				2'd02:
+					begin	//按s1所调时间类型减少一个单位
+					case(change_which)
+					2'd0:begin
+						if(hour==0)
+						hour=23;
+						else
+						hour=hour-1;
+					end
+					2'd1:begin
+						if(min==0)
+						min=59;
+						else
+						min=min-1;
+					end
+					2'd2:begin
+						if(second==0)
+						second=59;
+						else
+						second=second-1;
+					end
+					default : second=second;
+					endcase
+					end
+				2'd05:
+					begin	//按s4所调时间类型增加一个单位
+					case(change_which)
+					2'd0:begin
+						if(hour==23)
+						hour=0;
+						else
+						hour=hour+1;
+					end
+					2'd1:begin
+						if(min==59)
+						min=0;
+						else
+						min=min+1;
+					end
+					2'd2:begin
+						if(second==59)
+						second=0;
+						else
+						second=second+1;
+					end
+					default : second=second;
+					endcase
+					end
+				default :
+				begin
+				second=second;
+				min=min;
+				hour=hour;
+				end
+				endcase
 		end
+	end
 
+	///////////////////////////////////////////////////////////////    手动调整时间部分
+	always  @(posedge clk)	//削抖，不超过20ms的按键信号会被过滤
+	begin
+		if (key==5'b00000)
+		cnt_xd<=0;
+		else if (cnt_xd==xd)
+		cnt_xd<=xd;
+		else
+		cnt_xd<=cnt_xd+1;
+	end
+
+	always @(posedge clk)	//辨识按下的按键标号
+	begin
+		if(cnt_xd==0)
+		key_data<=0;
+		else if (cnt_xd==(xd-21'b1))
+			case(key)
+				5'b10000:key_data<=5;
+				5'b01000:key_data<=4;
+				5'b00100:key_data<=3;
+				5'b00010:key_data<=2;
+				5'b00001:key_data<=1;
+			endcase
+		else
+		key_data<=0;
+	end
+
+	always @(posedge clk)		//两状态切换//未来想改成多状态切换的状态机
+	begin
+		if(key_data==2'd01)
+			state<=~state;
+		else
+			state<=state;
+	end
+
+	always @(posedge clk)	//change_which决定调整时间类型，时？分？秒？
+	begin
+		if (state==1) begin
+			if(key_data==3)
+			begin
+				change_which=change_which+1;
+				if (change_which==3)
+				change_which=0;
+				else
+				change_which=change_which;
+			end
+			else
+			change_which=change_which;
+		end
+		else
+		change_which=0;
+	end
+
+	///////////////////////////////////////////////////////////////    数码管显示部分
 	always @(posedge clk)	//数码管扫描//频率500Hz//周期2us
 	begin
 		if (cnt_sm==18'd200000)
@@ -97,31 +223,79 @@ module clock(
 		end
 	end
 
-	always @(posedge clk)
+	always @(posedge clk)	//位于"调整时间状态时"shine控制调整时间时数码管的闪烁,shine=0表示亮起，闪烁周期为0.5s,正常状态不启动
+	begin
+		if(state==1)		//后续开发闹钟也要闪烁，到时候再加
+		begin
+			if(cnt_trinkle==26'd50000000)
+			begin
+				cnt_trinkle<=0;
+				shine<=~shine;
+			end
+			else
+			cnt_trinkle<=cnt_trinkle+1;
+		end
+		else begin
+			cnt_trinkle<=0;
+			shine<=0;
+		end
+	end
+
+	always @(posedge clk)	//将十进制的cnt_which,译成二进制的which,用以选择显示的数码管//考虑shine闪烁模块的影响
 	begin
 		case (cnt_which)
 			3'd0:begin
+				if (shine==0)	//平时时钟模式shine=0表示亮起
+				which<=8'b10000000;
+				else if(change_which==0)	//此时shine！=0表示调整时间状态一定开启，控制熄灭
+				which<=8'b00000000;
+				else
 				which<=8'b10000000;
 			end
 			3'd1:begin
+				if (shine==0)	//平时时钟模式shine=0表示亮起
+				which<=8'b01000000;
+				else if(change_which==0)	//此时shine！=0表示调整时间状态一定开启，控制熄灭
+				which<=8'b00000000;
+				else
 				which<=8'b01000000;
 			end
 			3'd2:begin
-				which<=8'b00100000;
+				which<=8'b00100000;		//这一位为分割符，不需要闪烁
 			end
 			3'd3:begin
+				if (shine==0)	//平时时钟模式shine=0表示亮起
+				which<=8'b00010000;
+				else if(change_which==1)	//此时shine！=0，表示调整时间状态一定开启，控制熄灭
+				which<=8'b00000000;
+				else
 				which<=8'b00010000;
 			end
 			3'd4:begin
+				if (shine==0)	//平时时钟模式shine=0表示亮起
+				which<=8'b00001000;
+				else if(change_which==1)	//此时shine！=0，表示调整时间状态一定开启，控制熄灭
+				which<=8'b00000000;
+				else
 				which<=8'b00001000;
 			end
 			3'd5:begin
-				which<=8'b00000100;
+				which<=8'b00000100;		//这一位为分割符，不需要闪烁
 			end
 			3'd6:begin
+				if (shine==0)	//平时时钟模式shine=0表示亮起
+				which<=8'b00000010;
+				else if(change_which==2)	//此时shine！=0，表示调整时间状态一定开启，控制熄灭
+				which<=8'b00000000;
+				else
 				which<=8'b00000010;
 			end
 			3'd7:begin
+				if (shine==0)	//平时时钟模式shine=0表示亮起
+				which<=8'b00000001;
+				else if(change_which==2)	//此时shine！=0，表示调整时间状态一定开启，控制熄灭
+				which<=8'b00000000;
+				else
 				which<=8'b00000001;
 			end
 			default :  which<=8'b00000000;
@@ -142,6 +316,7 @@ module clock(
 			default : num<=0;
 		endcase
 	end
+
 
 	always @(posedge clk)	//数码管段码翻译部分，将需要显示的数字转化成数码管相对应的段码
 	begin
