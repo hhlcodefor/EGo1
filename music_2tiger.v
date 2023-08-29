163dcfbe41b21fb505f0e1d807a363c46acf8f6e
hhlcodefor <huhao2336289144@qq.com>
2023-08-29 18:59:42 +0800

    Create music_2tiger.v

---
+++ music_2tiger.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/08/29 18:58:22
+// Design Name: 
+// Module Name: music_2tiger
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module music_2tiger(
+
+    );
+endmodule
